2f067f0e47e5c33396af540b1a5c2f4e2577907c
CaioLr <56622666+CaioLr@users.noreply.github.com>
2025-11-02 21:12:10 -0300

    style(sections): Adjusting responsive mobile

--- app/src/sections/InitialSection.jsx
+++ app/src/sections/InitialSection.jsx
@@ -5,15 +5,15 @@ const InitialSection = () => {
   return (
     <section className='relative w-full h-screen flex flex-col items-center justify-start border-b border-black'>
 
-      <div className='flex flex-col w-full h-1/4 justify-end gap-4 mt-14 lg:mt-20 p-10 lg:px-40 xl:px-60'>
+      <div className='flex flex-col w-full h-1/4 justify-end gap-4 mt-14 lg:mt-20 px-10 py-6 lg:px-40 xl:px-60'>
           <p className='comic-neue-bold text-2xl md:text-3xl lg:text-4xl'>Hello! I'm Caio,</p>
           <p className='comic-neue-regular text-xl md:text-2xl lg:text-3xl'>This is a portfolio showcasing the work and passion of a software engineer.</p>
       </div>
 
-      <div className='flex flex-col lg:flex-row w-full h-2/4 p-10 gap-4'>
+      <div className='flex flex-col lg:flex-row w-full h-2/4 px-10 sm:p-10 gap-4'>
 
-        <div className='w-full h-3/4 lg:h-full'><video className='object-cover w-full h-full' autoPlay loop muted playsInline src="/videos/test.mp4"></video></div>
-        <div className='w-full h-3/4 lg:h-full'><video className='object-cover w-full h-full' autoPlay loop muted playsInline src="/videos/videoexample.mp4"></video></div>
+        <div className='w-full h-3/4 lg:h-full border-2'><video className='object-cover w-full h-full' autoPlay loop muted playsInline src="/videos/test.mp4"></video></div>
+        <div className='w-full h-3/4 lg:h-full border-2'><video className='object-cover w-full h-full' autoPlay loop muted playsInline src="/videos/videoexample.mp4"></video></div>
 
       </div>
 
